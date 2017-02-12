2af678ea10890a06fdcfc2d35d32585e72170f88
Kieran Porter <k.porter.phys@gmail.com>
2017-02-12 15:44:24 +0000

    Added a few nice output statements

--- testMuon.py
+++ testMuon.py
@@ -20,27 +20,28 @@ def main():
 
     for i in range (n_repeats):
 
+        print("Run {0} begining".format(i))
         muon_t = [muon_pdf.pullRandMuon() for j in range(n_points)]
         average_tau.append(np.mean(muon_t))
-    
+        print("Run {0} ending".format(i))
 
     # Output the relevant information to the user.
 
     standard_deviation = np.std(average_tau)
     std_err_mean = standard_deviation/np.sqrt(n_repeats)
-    
 
-    print("Mean muon lifetime: ", np.mean(average_tau), "\n")
-    print("Standard deviation: ", standard_deviation, "\n")
-    print("Standard error on mean: ", std_err_mean)
-   
+
+    print("Mean muon lifetime: {0:6.3f}".format(np.mean(average_tau)))
+    print("Standard deviation: {0:6.3f}".format(standard_deviation))
+    print("Standard error on mean: {0:6.4f}".format(std_err_mean))
+
     # Plot and show a histogram of the data.
 
     plt.hist(average_tau, 10)
     plt.xlabel("Lifetime (ms)")
     plt.ylabel("Counts")
     plt.show()
-    
+
 
 # Run main.
 
