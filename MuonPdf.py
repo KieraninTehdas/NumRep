46d31320394a3c9a88870c76fafa4a9239b0f430
Kieran Porter <k.porter.phys@gmail.com>
2017-02-12 17:18:24 +0000

    Added a few finishing touches!

--- MuonPdf.py
+++ MuonPdf.py
@@ -1,4 +1,5 @@
-#Class to implement a random Gaussian generator
+#Class to implement a random number generator from an exponential
+# muon lifetime probability distribution.
 
 import math
 import matplotlib.pyplot as plt
@@ -13,14 +14,14 @@ class MuonPdf():
 
         self.tau = tau
         self.normalisation = 1.0/self.tau
-        
+
 
         # Function to evaluate the pdf at point t_point.
-       
+
     def evaluateMuonPdf(self, t_point):
 
         result = self.normalisation * np.exp((-1.0*t_point)/self.tau)
-        
+
         return result
 
         # Function gives the max of pdf.
@@ -30,8 +31,8 @@ class MuonPdf():
         fmax = self.evaluateMuonPdf(0.0)
 
         return fmax
-        
-        # Function to generate random number from exponential pdf. 
+
+        # Function to generate random number from exponential pdf.
 
     def pullRandMuon(self, upper_bound = 20.0):
 
@@ -39,28 +40,14 @@ class MuonPdf():
         evaluated_random = 0.0
         scaled_random = 0.0
         random_point = 1.0
-        
+
         while (random_point > evaluated_random):
 
             random_x = np.random.uniform()
             scaled_random_x = lower_bound + (upper_bound - lower_bound)*random_x
             evaluated_random = self.evaluateMuonPdf(scaled_random_x)
-        
+
             random_point = np.random.uniform()*self.fmax()
-        
+
 
         return scaled_random_x
-
-    
-
-        
-
-        
-
-        
-
-        
-
-
-
-
--- testMuon.py
+++ testMuon.py
@@ -16,19 +16,37 @@ def main():
     muon_pdf = MuonPdf()
     average_tau= []
 
+
+    # Single test run to check exponential distribution statistics.
+
+    muon_t_test = [muon_pdf.pullRandMuon() for j in range(n_points)]
+    print("Single run mean lifetime: {0:6.3f}".format(
+        np.mean(muon_t_test)))
+    print("Single run standard deviation: {0:6.4f}".format(
+        np.std(muon_t_test)))
+    print("Single run error on mean: {0:6.4f}".format(
+        np.std(muon_t_test)/np.sqrt(n_points)))
+
+    plt.hist(muon_t_test, 50)
+    plt.xlabel("Lifetime (ms)")
+    plt.ylabel("Counts")
+    plt.show()
+
+
+
     # Perform 500 runs of 1000 events each. Average each run and add to average list.
 
     for i in range (n_repeats):
 
-        print("Run {0} begining".format(i))
+        print("Run {0} begins!".format(i))
         muon_t = [muon_pdf.pullRandMuon() for j in range(n_points)]
         average_tau.append(np.mean(muon_t))
-        print("Run {0} ending".format(i))
+        print("Run {0} ends!".format(i))
 
     # Output the relevant information to the user.
 
     standard_deviation = np.std(average_tau)
-    std_err_mean = standard_deviation/np.sqrt(n_repeats)
+    std_err_mean = standard_deviation/np.sqrt(n_points)
 
 
     print("Mean muon lifetime: {0:6.3f}".format(np.mean(average_tau)))
