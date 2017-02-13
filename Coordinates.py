07171aa0a6d92c3881359eaf9843697046989e00
Kieran Porter <s1216429@cplab047.ph.ed.ac.uk>
2017-02-13 12:54:27 +0000

    Maybe helpful class to store coordinates

---
+++ Coordinates.py
@@ -0,0 +1,12 @@
+
+# Class to hold pairs of co-ordinates (x,y) in a list.
+
+class Coordinates():
+
+    def __init__(self):
+
+        self.coordinates = [ ]
+
+    def add_coord(self, new_pair):
+
+        self.coordinates.append(new_pair)
