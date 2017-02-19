e83dd75848c232bd8ba82486bf8642142b2b5dcd
kporter <k.porter.phys@gmail.com>
2017-02-19 17:10:08 +0000

    Add files via upload

--- Coordinates.py
+++ Coordinates.py
@@ -10,3 +10,23 @@ class Coordinates():
     def add_coord(self, new_pair):
 
         self.coordinates.append(new_pair)
+
+    def get_coord(self, point):
+
+        if((point < 0) or (point > len(self.coordinates))):
+            print("Index out of range! Returning (0,0)...")
+            return [0.0, 0.0]
+
+        return self.coordinates[point]
+
+    def get_all_coordiante(self):
+
+        return self.coordinates
+
+    def get_x_values(self):
+
+        return [x for [x,y] in self.coordinates]
+
+    def get_y_values(self):
+
+        return [y for [x,y] in self.coordinates]
--- testODE.py
+++ testODE.py
@@ -1,20 +1,33 @@
 from ODE import *
+from Coordinates import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
 
     exponential_ode = ExponentialODE()
 
-    
-        
+
+
     print(exponential_ode.first_derivative([0.0, 5.0]))
     print(exponential_ode.initial_value())
     print(exponential_ode.exact_solution(0))
 
-    
-    polynomial_ode = PolynomialODE([5.0, 2.0, 3.0, 4.0])
+    exponentials = [[i, exponential_ode.exact_solution(i)] for i in range(-50,50)]
+
+    print("Value of function at {0} is {1}".format(exponentials[0][1], exponentials[1]))
+
+
+    polynomial_ode = PolynomialODE([1.0, -2.0, -3.0, -4.0])
 
     print(polynomial_ode.first_derivative([1.0,1.0]))
     print(polynomial_ode.initial_value())
+
+    polynomials = [[i,polynomial_ode.exact_solution(i)] for i in range(-50,50)]
+    print(polynomials)
+
+    print("Value of function at {0} is {1}".format(polynomials[0][1], polynomials[1]))
+
     print(polynomial_ode.exact_solution(0))
 
     sinusoidal_ode = SinusoidalODE()
@@ -22,5 +35,13 @@ def main():
     print(sinusoidal_ode.first_derivative([1.0,1.0]))
     print(sinusoidal_ode.initial_value())
     print(sinusoidal_ode.exact_solution(1.56))
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(*zip(*polynomials))
+
+    plt.subplot(212)
+    plt.plot(*zip(*exponentials))
+    plt.show()
 
 main()
